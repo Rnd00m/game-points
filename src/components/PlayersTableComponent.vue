9517c1fc9516d87f727b9d5deda4071644ae5958
Julien BOILEVIN <j.boilevin@lepermislibre.fr>
2023-02-06 13:58:24 +0100

    feat: add color picker

--- src/components/PlayersTableComponent.vue
+++ src/components/PlayersTableComponent.vue
@@ -50,6 +50,21 @@ const counterStore = useCounterStore();
             class="input input-bordered w-full max-w-xs"
           />
         </div>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Color</span>
+          </label>
+          <div class="grid grid-cols-8 gap-3">
+            <div class="h-8" style="background-color: #1abc9c"></div>
+            <div class="h-8" style="background-color: #2ecc71"></div>
+            <div class="h-8" style="background-color: #3498db"></div>
+            <div class="h-8" style="background-color: #9b59b6"></div>
+            <div class="h-8" style="background-color: #2c3e50"></div>
+            <div class="h-8" style="background-color: #f1c40f"></div>
+            <div class="h-8" style="background-color: #e67e22"></div>
+            <div class="h-8" style="background-color: #e74c3c"></div>
+          </div>
+        </div>
       </label>
     </label>
   </div>
